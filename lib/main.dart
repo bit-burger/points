85e3dcc3b1aca8902a9b3c8106e3c2293ed3453c
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-09-30 14:23:27 +0200

    Completed configuration of supabase (via .env)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:points/points.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:points/supabase_configuration.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+  await configureSupabase();
+
   runApp(Points());
 }
--- lib/supabase_configuration.dart
+++ lib/supabase_configuration.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/foundation.dart' as flutter;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+Future<void> configureSupabase() async {
+  final env = dotenv.env;
+  final String? supabaseUrl = env["SUPABASE_URL"],
+      supabaseAnonKey = env["SUPABASE_ANON_KEY"];
+
+  assert(supabaseUrl != null, "The supabase url is missing");
+  assert(supabaseAnonKey != null, "The supabase anon key is missing");
+
+  assert(
+    RegExp(r"^https?:\/\/.+\..+").hasMatch(supabaseUrl!),
+    "The supabase url ($supabaseUrl) is invalid",
+  );
+
+  assert(
+    RegExp(r"^(\.|\w){147}$").hasMatch(supabaseAnonKey!),
+    "The supabase anon key ($supabaseAnonKey) is invalid",
+  );
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+    debug: !flutter.kReleaseMode, // optional
+  );
+}
