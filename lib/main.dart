5c3c4d3382c49d45d9bfc415647bb127d6154032
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-10-09 16:15:44 +0200

    Added the Courier Prime font

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:points/points.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:points/supabase_configuration.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  GoogleFonts.config.allowRuntimeFetching = false;
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load(fileName: ".env");
