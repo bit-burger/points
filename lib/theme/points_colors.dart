4341eb6200d43ee130767b1dfd7354128cb52bab
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-10-09 16:18:53 +0200

    Revamped colors and themes and add the font to the theme

---
+++ lib/theme/points_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+const List<Color> colors = [
+  red,
+  orange,
+  yellow,
+  green,
+  cyan,
+  blue,
+  darkBlue,
+  purple,
+  pink,
+  white,
+];
+
+const red = Color(0xFFFFADAD);
+const orange = Color(0xFFFFD6A5);
+const yellow = Color(0xFFFDFFB6);
+const green = Color(0xFFCAFFBF);
+const cyan = Color(0xFFB3FBDF);
+const blue = Color(0xFFAAE0EF);
+const darkBlue = Color(0xFFA0C4FF);
+const purple = Color(0xFFBDB2FF);
+const pink = Color(0xFFEFADFF);
+const white = Color(0xFFDDE6E8);
+
+final errorColor = Colors.red.shade700;
