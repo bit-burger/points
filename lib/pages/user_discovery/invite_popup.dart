8693efaece17ec6242332e40026b5a18ea011348
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-11-22 17:51:41 +0100

    Implement feature to add people by their email address

---
+++ lib/pages/user_discovery/invite_popup.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:points/helpers/uppercase_to_lowercase_text_input_formatter.dart';
+import 'package:points/state_management/auth/auth_cubit.dart';
+import 'package:points/state_management/user_discovery/email_user_inviter_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:points/widgets/loader.dart';
+import 'package:points/widgets/neumorphic_loading_text_button.dart';
+import 'package:points/widgets/neumorphic_text_form_field.dart';
+import 'package:user_repositories/relations_repository.dart';
+import 'package:user_repositories/user_discovery_repository.dart';
+import '../../helpers/reg_exp.dart' as regExp;
+
+class InvitePopup extends StatelessWidget {
+  Widget _buildTextField(BuildContext context) {
+    return BlocBuilder<EmailUserInviterCubit, EmailUserInviterState>(
+      buildWhen: (oldState, newState) {
+        return newState is! EmailUserInviterLoading;
+      },
+      builder: (context, state) {
+        String? error;
+        if (state is EmailUserInviterError) {
+          error = state.message;
+        }
+        return NeumorphicTextFormField(
+          errorText: error,
+          hintText: "email",
+          onFieldSubmitted:
+              state is! EmailUserInviterFound ? null : (s) => _submit(context),
+          onChanged: (s) {
+            context
+                .read<EmailUserInviterCubit>()
+                .updateSearchQuery(searchQuery: s);
+          },
+          inputFormatters: [
+            FilteringTextInputFormatter(
+              regExp.emailFilter,
+              allow: true,
+            ),
+            UppercaseToLowercaseTextInputFormatter(),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return BlocBuilder<EmailUserInviterCubit, EmailUserInviterState>(
+      builder: (context, state) {
+        final loading = state is EmailUserInviterRequestLoading;
+        final disabled = state is EmailUserInviterError ||
+            state is EmailUserInviterLoading ||
+            state is EmailUserInviterInitial;
+        return Center(
+          child: NeumorphicLoadingTextButton(
+            onPressed: disabled ? null : () => _submit(context),
+            padding: EdgeInsets.zero,
+            child: SizedBox(
+              height: 56,
+              child: Center(child: Text("Request")),
+            ),
+            loader: SizedBox(
+              height: 56,
+              child: Loader(),
+            ),
+            loading: loading,
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<EmailUserInviterCubit>(
+      create: (context) {
+        final credentials =
+            (context.read<AuthCubit>().state as LoggedInState).credentials;
+        return EmailUserInviterCubit(
+          credentials: credentials,
+          userDiscoveryRepository: context.read<UserDiscoveryRepository>(),
+          relationsRepository: context.read<RelationsRepository>(),
+        );
+      },
+      child: Neumorphic(
+        child: Column(
+          children: [
+            BlocBuilder<EmailUserInviterCubit, EmailUserInviterState>(
+              buildWhen: (oldState, newState) {
+                return newState is EmailUserInviterFinished ||
+                    newState is EmailUserInviterInitial;
+              },
+              builder: (context, state) {
+                late final Widget widget;
+                if (state is EmailUserInviterFinished) {
+                  widget = Column(
+                    key: ValueKey("initial"),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 32),
+                      RichText(
+                        text: TextSpan(
+                          style:
+                              Theme.of(context).textTheme.headline6!.copyWith(
+                                    color: Theme.of(context).disabledColor,
+                                  ),
+                          children: [
+                            WidgetSpan(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 4),
+                                child: Icon(
+                                  Icons.check,
+                                  color: Theme.of(context).disabledColor,
+                                ),
+                              ),
+                            ),
+                            TextSpan(text: "user successfully added"),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: NeumorphicLoadingTextButton(
+                          onPressed: () {
+                            context.read<EmailUserInviterCubit>().reset();
+                          },
+                          child: Text("Add another user"),
+                        ),
+                      ),
+                    ],
+                  );
+                } else {
+                  widget = Column(
+                    key: ValueKey("form"),
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(height: 24),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 24),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Add friend by email",
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      SizedBox(
+                        height: 90,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: _buildTextField(context),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: _buildButton(context),
+                      ),
+                    ],
+                  );
+                }
+                return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 500),
+                  child: widget,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submit(BuildContext context) {
+    context.read<EmailUserInviterCubit>().requestUser();
+  }
+}
---
+++ lib/state_management/user_discovery/email_user_inviter_cubit.dart
@@ -0,0 +1,85 @@
+import 'package:auth_repository/auth_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:user_repositories/relations_repository.dart';
+import 'package:user_repositories/user_discovery_repository.dart';
+import 'package:async/async.dart';
+import '../../helpers/reg_exp.dart' as regExp;
+import 'package:equatable/equatable.dart';
+
+part 'email_user_inviter_state.dart';
+
+class EmailUserInviterCubit extends Cubit<EmailUserInviterState> {
+  final UserDiscoveryRepository _userDiscoveryRepository;
+  final RelationsRepository _relationsRepository;
+  final AccountCredentials _credentials;
+
+  EmailUserInviterCubit({
+    required AccountCredentials credentials,
+    required UserDiscoveryRepository userDiscoveryRepository,
+    required RelationsRepository relationsRepository,
+  })  : _credentials = credentials,
+        _userDiscoveryRepository = userDiscoveryRepository,
+        _relationsRepository = relationsRepository,
+        super(EmailUserInviterInitial());
+
+  void reset() {
+    emit(EmailUserInviterInitial());
+  }
+
+  void requestUser() async {
+    assert(state is EmailUserInviterFound);
+
+    _relationsRepository.request((state as EmailUserInviterFound).userId);
+
+    emit(EmailUserInviterRequestLoading());
+
+    final relationsQueue = StreamQueue(_relationsRepository.relationsStream);
+    try {
+      await relationsQueue.next;
+      emit(EmailUserInviterFinished());
+    } on PointsConnectionError catch (e) {
+      emit(EmailUserInviterError(e.message));
+    }
+  }
+
+  void updateSearchQuery({required String searchQuery}) {
+    if (searchQuery.isEmpty) {
+      emit(EmailUserInviterInitial());
+    } else if (!regExp.email.hasMatch(searchQuery)) {
+      emit(EmailUserInviterNotValid());
+    } else {
+      _searchForUser(searchQuery);
+    }
+  }
+
+  void _searchForUser(String email) async {
+    emit(EmailUserInviterLoading());
+    try {
+      final foundUser =
+          await _userDiscoveryRepository.getUserByEmail(email: email);
+      if (foundUser == null) {
+        emit(EmailUserInviterNotFound());
+      } else {
+        _handleUserExists(foundUser, email);
+      }
+    } on PointsConnectionError catch (e) {
+      emit(EmailUserInviterError(e.message));
+    }
+  }
+
+  void _handleUserExists(User foundUser, String email) {
+    final relationsAlreadyExists = _relationsRepository
+            .currentUserRelations!.all
+            .indexWhere((user) => user.id == foundUser.id) !=
+        -1;
+    final isSelf = _credentials.email == email;
+    if (relationsAlreadyExists) {
+      emit(EmailUserInviterFoundUserIsAlreadyRelated());
+    } else if (isSelf) {
+      emit(EmailUserInviterFoundUserIsSelf());
+    } else {
+      emit(EmailUserInviterFound(foundUser.id));
+    }
+  }
+}
